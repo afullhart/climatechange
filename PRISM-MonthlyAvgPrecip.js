c04ab169e5a6b8c2d9b21ba9794f127180d32541
afullhart <58483164+afullhart@users.noreply.github.com>
2023-08-09 17:50:08 -0700

    Update PRISM-MonthlyAvgPrecip.js

--- PRISM-MonthlyAvgPrecip.js
+++ PRISM-MonthlyAvgPrecip.js
@@ -1,6 +1,7 @@
 var path = 'users/gponce/usda_ars/image_collections/prism800m_monthly_precipitation'; //'b1'
 
 var dataset = ee.ImageCollection(path).select('b1');
+var bounds = dataset.geometry().bounds();
 var proj=dataset.first().projection();
 var start_year = 1974;
 var end_year = 2013;
@@ -33,12 +34,13 @@ var out_dir = 'users/andrewfullhart/';
 
 for (var i = 0; i < 12; i++) {
   var m = i + 1;
-  Export.image.toAsset({image:ee.Image(results.get(i))
+  Export.image.toAsset({image: ee.Image(results.get(i))
                                     .rename(['prcp_'+ m]), 
                         description: 'prcp_' + m + '_1974_2013', 
                         assetId: out_dir + 'prcp_' + m + '_1974_2013',
-                        scale:proj.nominalScale().getInfo(), 
+                        region: bounds, 
+                        scale: proj.nominalScale().getInfo(), 
                         crsTransform: [0.00833333333, 0, -125.02083333, 0, 0.00833333333, 24.0625],
                         crs: 'EPSG:4326',
-                        maxPixels:1e13});
+                        maxPixels: 1e13});
 }
