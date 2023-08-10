13998f456ddd72c0812636f098895a7dd41b26b7
afullhart <58483164+afullhart@users.noreply.github.com>
2023-08-10 16:06:37 -0700

    Update and rename Slope_Avg.js to Slope_Avg_Step_One.js

--- Slope_Avg_Step_One.js
+++ Slope_Avg_Step_One.js
@@ -5,21 +5,11 @@ var dem_dataset = ee.Image('USGS/3DEP/10m');
 var elevation = dem_dataset.select('elevation');
 var bounds = elevation.geometry().bounds();
 var slope = ee.Terrain.slope(elevation);
-
-var slopeAvg = slope
-    .reduceResolution({
-      reducer: ee.Reducer.mean(),
-      maxPixels: 65535
-    })
-    .reproject({
-      crs: coarseProjection
-    });
-
 var out_dir = 'users/andrewfullhart/';
 
-Export.image.toAsset({image: slopeAvg,
-                      description: 'AvgSlope3DEP',
-                      assetId: out_dir + 'AvgSlope3DEP',
+Export.image.toAsset({image: slope,
+                      description: 'SlopeHiRes',
+                      assetId: out_dir + 'SlopeHiRes',
                       region: bounds, 
                       scale: coarseProjection.nominalScale().getInfo(), 
                       crsTransform: [0.00833333333, 0, -125.02083333, 0, 0.00833333333, 24.0625],
