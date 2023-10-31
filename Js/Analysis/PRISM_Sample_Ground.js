c15d0f9dbfe07538c3f6c692530f4ac94953fa3e
afullhart <58483164+afullhart@users.noreply.github.com>
2023-10-30 17:43:08 -0700

    Update PRISM_Sample_Ground.js

--- Js/Analysis/PRISM_Sample_Ground.js
+++ Js/Analysis/PRISM_Sample_Ground.js
@@ -1,1 +1,29 @@
+var path = 'users/gponce/usda_ars/image_collections/prism800m_monthly_precipitation'; //'b1'
+var dataset = ee.ImageCollection(path);
+var points = ee.FeatureCollection('users/andrewfullhart/US_CLIGEN_Coords');
+
+var dataset = ee.ImageCollection(path).select('b1');
+var bounds = dataset.geometry().bounds();
+var proj = dataset.first().projection();
+var start_year = 1974;
+var end_year = 2013;
+var start = ee.Date.fromYMD(start_year, 1, 1);
+var end = ee.Date.fromYMD(end_year, 1, 1);
+var years = ee.List.sequence(start_year, end_year);
+
+var sum_image = dataset.filterDate(start, end).sum().divide(40);
+print(sum_image);
+print(points);
+Map.addLayer(sum_image);
+
+var sample_fc = sum_image.sampleRegions(points);
+print(sample_fc);
+
+Export.table.toDrive({collection:sample_fc,
+                      description:'allmodelsJul',
+                      selectors:['stationID', 'b1'],
+                      folder:'GEE_Downloads'
+});
+
+
 
