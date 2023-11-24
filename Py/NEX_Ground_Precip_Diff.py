213d3e1c4b2c141b8d422316ae48c59785363f02
afullhart <58483164+afullhart@users.noreply.github.com>
2023-11-23 22:56:55 -0700

    Update NEX_Ground_Precip_Diff.py

--- Py/NEX_Ground_Precip_Diff.py
+++ Py/NEX_Ground_Precip_Diff.py
@@ -1,40 +1,45 @@
 import numpy as np
 
-USCLIGENf = r'C:\Users\afullhart\Desktop\USCLIGEN_Annual_Precip.csv'
-NEXf = r'C:\Users\afullhart\Desktop\NEX_USCLIGEN_Map_Sample_Annual_Precip.csv'
+usCLIGENf = '/content/drive/My Drive/Colab Notebooks/USCLIGEN_Annual_Precip.csv'
+nexf = '/content/drive/My Drive/Colab Notebooks/NEX_USCLIGEN_Map_Sample_Annual_Precip.csv'
+outf = '/content/drive/My Drive/Colab Notebooks/NEX_Ground_Precip_Diff.csv'
 
-
-US_Dict = {}
-with open(USCLIGENf) as f:
+us_dict = {}
+with open(usCLIGENf) as f:
     next(f)
     for line in f:
         row = line.strip('\n').split(',')
-        US_Dict[row[0]] = float(row[1])
+        us_dict[row[0]] = float(row[1])
 
-NEX_Dict = {}
-with open(NEXf) as f:
+nex_dict = {}
+with open(nexf) as f:
     next(f)
     for line in f:
         row = line.strip('\n').split(',')
         modelID = row[0]
         row = line.split(']","[')[0].split('"[')[1].split(',')
-        StationID_List = [s.strip() for s in row]
+        stationID_list = [s.strip() for s in row]
         row = line.split(']","[')[1].strip(']"\n')
         datarow = [float(x)/25.4 for x in row.split(',')]
-        NEX_Dict[modelID] = {StationID_List[i]:datarow[i] for i, data in enumerate(datarow)}
+        nex_dict[modelID] = {stationID_list[i]:datarow[i] for i, data in enumerate(datarow)}
 
-Results_Dict = {}
-for modelID in NEX_Dict:
+results_dict = {}
+for modelID in nex_dict:
     print(modelID)
-    absperror = []
-    for stationID in StationID_List:
+    abspererr = []
+    sqrerr = []
+    for stationID in stationID_list:
         if stationID[:2] in ['az', 'nv', 'nm', 'ut']:
-            est = NEX_Dict[modelID][stationID]
-            obs = US_Dict[stationID]
-            #absperror.append(abs(est-obs)/obs*100)
-            absperror.append((est-obs)**2)
-            print(str(est), str(obs), str(abs(est-obs)/obs*100))
-    
-    Results_Dict[modelID] = np.mean(absperror)**0.5
-    #Results_Dict[modelID] = np.mean(absperror)
-          
+            est = nex_dict[modelID][stationID]
+            obs = us_dict[stationID]
+            abspererr.append(abs(est-obs)/obs*100)
+            sqrerr.append((est-obs)**2)
+
+    results_dict[modelID] = [str(np.mean(abspererr)), 
+                             str(np.mean(sqrerr)**0.5)]
+
+with open(outf, 'w') as fo:
+  fo.write('stationID,abspererr,rmse\n')
+  for key in results_dict:
+      fo.write(','.join([key, results_dict[key][0], results_dict[key][1]]) + '\n')
+
