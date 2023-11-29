e26565be523c9a31693b6e2afe4263b2ec1997b2
afullhart <58483164+afullhart@users.noreply.github.com>
2023-11-28 23:27:12 -0700

    Update NEX_Ground_Precip_Diff.py

--- Py/NEX_Ground_Precip_Diff.py
+++ Py/NEX_Ground_Precip_Diff.py
@@ -1,37 +1,42 @@
 import numpy as np
 import shutil
 import os
-baseDIR = '/content/driver/My Drive/Colab Notebooks'
+baseDIR = '/content/drive/My Drive/Colab Notebooks'
+inFOLDER = 'Script Input Files'
 outFOLDER = 'Output_NEX_Ground_Precip_Diff'
 
 if os.path.exists(os.path.join(baseDIR, outFOLDER)):
   shutil.rmtree(os.path.join(baseDIR, outFOLDER))
 os.makedirs(os.path.join(baseDIR, outFOLDER))
 
-usCLIGENf = os.path.join(baseDIR, 'USCLIGEN_Annual_Precip.csv')
-nexf = os.path.join(baseDIR, 'NEX_USCLIGEN_Map_Sample_Annual_Precip.csv')
+#usGRNDf = os.path.join(baseDIR, inFOLDER, 'USCLIGEN_Annual_Precip.csv')
+usGRNDf = os.path.join(baseDIR, inFOLDER, 'GHCNd_Annual_Precip.csv')
+#nexf = os.path.join(baseDIR, inFOLDER, 'NEX_USCLIGEN_Map_Sample_Annual_Precip.csv')
+nexf = os.path.join(baseDIR, inFOLDER, 'NEX_GHCNd_Map_Sample_Annual_Precip.csv')
 outONEf = os.path.join(baseDIR, outFOLDER, 'NEX_Ground_Precip_Diff_Results.csv')
 outTWOf = os.path.join(baseDIR, outFOLDER, 'NEX_Ground_Precip_Diff_Datapoints.csv')
 outTHREEf = os.path.join(baseDIR, outFOLDER, 'bad.csv')
 
 #Has missing NEX sample value
-bad_list_one = ['az026132']
+bad_list_one = ['az026132', 'USC00026132']
 #Has suspect NEX sample value
 bad_list_two = ['ut424856','ut428119','ut425182','ut423809','ut420061','ut420072','ut427598','ut429595','ut425186','ut422726','ut422057','ut422385','ut426919','ut421759','ut427271','ut426869','ut428733','ut424467','ut427846']
-bad_list = bad_list_one + bad_list_two
-
+bad_list_three = ['USC00422726','USC00424856','USC00426919','USC00422057','USC00425186','USC00425194','USC00420819','USC00429346','USC00428973','USW00024127','USC00420061','USC00421590','USC00425892','USC00427165','USC00427846','USC00424467','USC00427271','USC00420820','USC00429595','USC00421759','USC00425826','USC00427064','USC00428828','USC00428119','USC00423809','USC00421446','USC00429165','USC00425182','USC00420072']
+#Has suspect GHCNd sample value
+bad_list_four = ['USC00027622', 'USC00025418']
+bad_list = bad_list_one + bad_list_two + bad_list_three + bad_list_four
 
 stationID_list = []
 us_dict = {}
-print(usCLIGENf)
-with open(usCLIGENf) as f:
+print(usGRNDf)
+with open(usGRNDf) as f:
   next(f)
   for line in f:
     row = line.strip('\n').split(',')
     stationID = row[0]
-    if stationID[:2] in ['az', 'nv', 'nm', 'ut'] and stationID not in bad_list:
+    if stationID[:2] in ['az', 'nv', 'nm', 'ut', 'US'] and stationID not in bad_list:
       stationID_list.append(row[0])
-      us_dict[row[0]] = float(row[1])*25.4
+      us_dict[row[0]] = float(row[1])
 
 nex_dict = {}
 with open(nexf) as f:
@@ -68,15 +73,13 @@ for model in results_dict:
   X = x[:,np.newaxis]
   a, b, c, d = np.linalg.lstsq(X, y)
 
-  #rmse = np.sqrt(np.mean((y-x)**2))
-  #print(rmse)
-
   resid = []
   for i, x_val in enumerate(x):
     resid.append(abs(y[i] - a*x_val))
   resid_std = np.std(resid, ddof=1)
 
-  rmse = np.sqrt(np.mean([(y[i]-x[i])**2 for i, r in enumerate(resid) if r < 2*resid_std]))
+  resid_std_thresh = 999
+  rmse = np.sqrt(np.mean([(y[i]-x[i])**2 for i, r in enumerate(resid) if r < resid_std_thresh*resid_std]))
   print(model)
   print(rmse)
 
@@ -91,6 +94,12 @@ with open(outTWOf, 'w') as fo:
   for stationID in datapts_dict['obs']:
     fo.write(stationID + ',' + ','.join([datapts_dict[key][stationID] for key in datapts_dict]) + '\n')
 
+"""
+with open(outTHREEf, 'w') as fo:
+  badd = set(badd)
+  print(len(badd))
+  fo.write(','.join(badd))
+"""
 
 
 
