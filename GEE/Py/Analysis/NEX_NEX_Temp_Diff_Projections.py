f3ea05bf5013292eea8e821be57b35906f8750b7
afullhart <58483164+afullhart@users.noreply.github.com>
2023-12-02 10:50:27 -0700

    Update NEX_NEX_Temp_Diff_Projections.py

--- GEE/Py/Analysis/NEX_NEX_Temp_Diff_Projections.py
+++ GEE/Py/Analysis/NEX_NEX_Temp_Diff_Projections.py
@@ -1,5 +1,11 @@
+import time
+import os
 import ee
 ee.Initialize()
+
+
+fileDIR = '/content/drive/My Drive/GEE_Downloads'
+parseFILE = '/content/drive/My Drive/Colab Notebooks/NEX_NEX_TEMP_DIFF_PROJECTIONS.csv'
 
 path = 'NASA/NEX-DCP30'#'pr kg/m^2/s'
 nex_set = ee.ImageCollection(path)
@@ -62,7 +68,7 @@ for i in range(len(model_loop_list)):
 
     ref_im = ee.ImageCollection(order_months.map(month_fn)).sum()
     ref_im = ref_im.expression('(tmax - 273.15)*(1.8)+32', {'tmax':ref_im.select('tasmax')})
-    
+
     def year_fn(year):
 
       start_nested = ee.Date.fromYMD(ee.Number(year), 1, 1)
@@ -124,3 +130,65 @@ for i in range(len(model_loop_list)):
                             selectors=['model', 'avg_tmax', 'avg_abs', 'avg_rel'])
 
   task.start()
+
+
+timer_condition = True
+while timer_condition is True:
+  thing = ee.data.listOperations()
+  timer_condition = False
+  for d in thing:
+    if d['metadata']['state'] in ['PENDING', 'RUNNING']:
+      timer_condition = True
+      break
+
+  time.sleep(5)
+
+time.sleep(60)
+
+
+files = os.listdir(fileDIR)
+
+if len(files) != 11:
+  print('WRONG FILE COUNT')
+
+lines = []
+for afile in files:
+  with open(os.path.join(fileDIR, afile)) as f:
+    flines = f.readlines()
+    for line in flines[1:]:
+      lines.append(line)
+
+results_dict = {}
+for line in lines:
+  row = line.split('"')
+  model = row[0].strip(',')
+  avg_prc = [str(s) for s in eval(row[1])]
+  avg_abs = [str(s) for s in eval(row[3])]
+  avg_rel = [str(s) for s in eval(row[5])]
+  results_dict[model] = [avg_prc, avg_abs, avg_rel]
+
+keys = []
+for key in results_dict:
+  keys.append(key)
+keys = sorted(keys, key=str.casefold)
+
+with open(parseFILE, 'w') as fo:
+  model_string = '_tmx,'.join(keys) + '_tmx,'
+  fo.write(model_string)
+  model_string = '_abs,'.join(keys) + '_abs,'
+  fo.write(model_string)
+  model_string = '_rel,'.join(keys) + '_rel\n'
+  fo.write(model_string)
+  for i in range(len(results_dict[keys[0]][0])):
+    for j in range(3):
+      for key in keys:
+        fo.write(results_dict[key][j][i])
+
+        if key != keys[-1]:
+          fo.write(',')
+        else:
+          if j != 2:
+            fo.write(',')
+
+    fo.write('\n')
+
