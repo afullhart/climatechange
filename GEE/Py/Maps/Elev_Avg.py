0190289c8efbd02f413de7017b0192043011891f
afullhart <58483164+afullhart@users.noreply.github.com>
2023-12-04 16:13:13 -0700

    Update Elev_Avg.py

--- GEE/Py/Maps/Elev_Avg.py
+++ GEE/Py/Maps/Elev_Avg.py
@@ -43,7 +43,7 @@ task = ee.batch.Export.image.toDrive(image=elevAvg,
           description='AvgElev3DEP',
           folder='GEE_Downloads',
           region=bounds,
-          scale=proj.nominalScale().getInfo(),
+          scale=coarseProjection.nominalScale().getInfo(),
           crsTransform=[0.00833333333, 0, -125.02083333, 0, 0.00833333333, 24.0625],
           crs='EPSG:4326',
           maxPixels=1e13)
