daf2c92d18ff32235441c982f52d771a851d8476
afullhart <58483164+afullhart@users.noreply.github.com>
2023-12-09 16:36:21 -0700

    Update NEX_Regional_Precip_1Year_Tseries.py

--- NotUsed/NEX_Regional_Precip_1Year_Tseries.py
+++ NotUsed/NEX_Regional_Precip_1Year_Tseries.py
@@ -1,11 +1,12 @@
+import time
 import os
 import ee
 ee.Initialize()
 
 path = 'NASA/NEX-DCP30'#'pr kg/m^2/s'
 ic = ee.ImageCollection(path)
-out_description = 'NEX_Regional_Avg_Precip_Tseries'
-model = 'CCSM4' #'CanESM2', 'MIROC5'
+outFILE = '/content/drive/My Drive/Colab Notebooks/NEX_Regional_1Year_Precip_Tseries.csv'
+model_list = ['CCSM4', 'CanESM2', 'MIROC5']
 study_area = ee.FeatureCollection('users/andrewfullhart/SW_Study_Area')
 
 bounds = ic.geometry().bounds()
@@ -23,40 +24,48 @@ modelfilter = ee.Filter.Or(
               ee.Filter.eq('scenario', 'historical'),
               ee.Filter.eq('scenario', 'rcp45'))
 
-ic = ic.filter(ee.Filter.eq('model', model))                                    \
-       .filter(modelfilter)                                                     \
-       .select('pr')
+out_dict = {}
+for model in model_list:
 
-def year_fn(year):
+  print(model)
 
-  start = ee.Date.fromYMD(ee.Number(year), 1, 1)
-  end = ee.Date.fromYMD(ee.Number(year), 12, 31)
-  year_ic = ic.filterDate(start, end)
+  model_ic = ic.filter(ee.Filter.eq('model', model))                            \
+               .filter(modelfilter)                                             \
+               .select('pr')
 
-  def month_fn(month):
-    mo_im = year_ic.filter(ee.Filter.calendarRange(month, month,'month'))       \
-                  .sum().multiply(86400)                                        \
-                  .multiply(ee.Number(ndays_months.get(ee.Number(month).subtract(1))))
-    return mo_im
+  def year_fn(year):
 
-  ann_im = ee.ImageCollection(order_months.map(month_fn)).sum()
+    start = ee.Date.fromYMD(ee.Number(year), 1, 1)
+    end = ee.Date.fromYMD(ee.Number(year), 12, 31)
+    year_ic = model_ic.filterDate(start, end)
 
-  mean_dict = ann_im.reduceRegion(
-    reducer=ee.Reducer.mean(),
-    geometry=study_area.geometry(),
-    scale=scale,
-    maxPixels=1e10)
-  avg_stat = mean_dict.get('pr')
+    def month_fn(month):
+      mo_im = year_ic.filter(ee.Filter.calendarRange(month, month,'month'))     \
+                    .sum().multiply(86400)                                      \
+                    .multiply(ee.Number(ndays_months.get(ee.Number(month).subtract(1))))
+      return mo_im
 
-  return ee.Feature(None, {'year':ee.Number(year), 'pr':avg_stat})
+    ann_im = ee.ImageCollection(order_months.map(month_fn)).sum()
+
+    mean_dict = ann_im.reduceRegion(
+      reducer=ee.Reducer.mean(),
+      geometry=study_area.geometry(),
+      scale=scale,
+      maxPixels=1e10)
+    avg_stat = mean_dict.get('pr')
+
+    return avg_stat
+  
+  model_precip_list = years_list.map(year_fn)
+  out_dict[model] = model_precip_list.getInfo()
 
 
-out_fc = ee.FeatureCollection(years_list.map(year_fn))
-
-task = ee.batch.Export.table.toDrive(collection=out_fc,
-                          description=out_description,
-                          folder='GEE_Downloads',
-                          selectors=['year', 'pr'])
-
-task.start()
-
+with open(outFILE, 'w') as fo:
+  fo.write('year,CCSM4,CanESM2,MIROC5\n')
+  for i, year in enumerate(years_list.getInfo()):
+    fo.write(str(year) + ',')
+    values = []
+    for model in model_list:
+      value = str(out_dict[model][i])
+      values.append(value)
+    fo.write(','.join(values) + '\n')
