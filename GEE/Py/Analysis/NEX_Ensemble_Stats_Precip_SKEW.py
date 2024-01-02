58c7e65f51cfedadda3fdcafe78fe0bffbb316e8
afullhart <58483164+afullhart@users.noreply.github.com>
2024-01-02 14:34:44 -0700

    Update NEX_Ensemble_Stats_Precip_SKEW.py

--- GEE/Py/Analysis/NEX_Ensemble_Stats_Precip_SKEW.py
+++ GEE/Py/Analysis/NEX_Ensemble_Stats_Precip_SKEW.py
@@ -114,7 +114,7 @@ with open(out_file, 'w') as fo:
     q75 = str(d['properties']['q75'])
     avg = str(d['properties']['avg'])
 
-    fo.write(','.join([yr, min, max, q25, q75, avg]) + '\n')
+    fo.write(','.join([yr, min, max, q25, q75, avg]))
 
   for model in model_list:
     fo.write(',' + str(d['properties'][model]))
