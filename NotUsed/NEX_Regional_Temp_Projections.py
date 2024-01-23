307d62269bb9961ef2b4522333aca7898fa0bf30
afullhart <58483164+afullhart@users.noreply.github.com>
2024-01-22 19:22:41 -0700

    Update NEX_Regional_Temp_Projections.py

--- NotUsed/NEX_Regional_Temp_Projections.py
+++ NotUsed/NEX_Regional_Temp_Projections.py
@@ -33,7 +33,7 @@ scale = 500
 start_year = 1974
 end_year = 2013
 start = ee.Date.fromYMD(start_year, 1, 1)
-end = ee.Date.fromYMD(end_year, 12, 31)
+end = ee.Date.fromYMD(end_year+1, 1, 1)
 ndays_months = ee.List([31, 28.25, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
 order_months = ee.List([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 
@@ -72,7 +72,7 @@ for i in range(len(model_loop_list)):
     def year_fn(year):
 
       start_nested = ee.Date.fromYMD(ee.Number(year), 1, 1)
-      end_nested = ee.Date.fromYMD(ee.Number(year).add(29), 12, 31)
+      end_nested = ee.Date.fromYMD(ee.Number(year).add(30), 1, 1)
       ic_nested = ic.filterDate(start_nested, end_nested)
 
       def month_fn(month):
