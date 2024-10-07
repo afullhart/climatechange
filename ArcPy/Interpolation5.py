e65cc9ddfa201c34c1bab8d171c6f18ea48f0ace
afullhart <58483164+afullhart@users.noreply.github.com>
2024-10-06 21:43:46 -0700

    Update Interpolation5.py

--- ArcPy/Interpolation5.py
+++ ArcPy/Interpolation5.py
@@ -1,1 +1,73 @@
+###############################################################################
+'Calculate PWW and PWD from RATIO, MEANP, and ACCM'
+###############################################################################
+
+import shutil
+import os
+import arcpy
+import pandas as pd
+import numpy as np
+
+gcmLabel = 'CCSM4'
+
+storeDIR = r'E:\Grid_Inputs\{}'.format(gcmLabel)
+elevDIR = r'E:\Grid_Inputs\DEM'
+featDIR = r'E:\Ground_Inputs'
+dataDIR = r'C:\Users\afullhart\Documents\ArcGIS\Projects\{}\Data'.format(gcmLabel)
+gdbDIR = r'C:\Users\afullhart\Documents\ArcGIS\Projects\{}\{}.gdb'.format(gcmLabel, gcmLabel)
+storeshpDIR = r'E:\Study_Area_Shp'
+maskSHP = os.path.join(dataDIR, 'Study_Area_Shp', 'Study_Area_Shp.shp')
+  
+if not os.path.exists(maskSHP):
+  shutil.copytree(storeshpDIR, os.path.join(dataDIR, 'Study_Area_Shp'))
+
+arcpy.env.workspace = gdbDIR
+arcpy.env.overwriteOutput = True
+
+var_labels = ['accm', 'mean', 'ratio']
+year_labels = ['_1974_2013_', '_2000_2029_', '_2010_2039_', '_2020_2049_', '_2030_2059_', '_2040_2069_', '_2050_2079_', '_2060_2089_', '_2070_2099_']
+
+extent = [43.0, -121.0, -102.0, 30.0]
+
+ndays = [31, 28.25, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+map_io_data = []
+for mo in range(1, 13):
+  for yrlabel in year_labels:
+    map_io_data.append([yrlabel, mo])
+
+for io in map_io_data:
+  
+  print(io)
+  yrlabel = io[0]
+  mo = io[1]
+  
+  outputCoordinateSystem = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'
+  parallelProcessingFactor = '5'
+  extent = '-121.0 30.0 -102.0 43.0 GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'
+
+  with arcpy.EnvManager(outputCoordinateSystem=outputCoordinateSystem, extent=extent, parallelProcessingFactor=parallelProcessingFactor):
+
+    accm = 'accm{}{}'.format(yrlabel, str(mo))
+    mean = 'mean{}{}'.format(yrlabel, str(mo))
+    ratio = 'ratio_1974_2013_{}'.format(str(mo))
+    pwd = 'pwd{}{}'.format(yrlabel, str(mo))
+    
+    output_raster = arcpy.sa.RasterCalculator(
+      rasters=[accm, mean, ratio],
+      input_names=['accm', 'mean', 'ratio'],
+      expression='Con(IsNull(accm), accm, 1/((1/(((accm/25.4)/mean)/{})) + ratio - 1))'.format(str(ndays[mo-1]))
+    )
+
+    output_raster.save('pwd{}{}'.format(yrlabel, str(mo)))
+
+    output_raster = arcpy.sa.RasterCalculator(
+      rasters=[accm, pwd, ratio],
+      input_names=['accm', 'pwd', 'ratio'],
+      expression='Con(IsNull(accm), accm, ratio*pwd)'
+    )
+
+    output_raster.save('pww{}{}'.format(yrlabel, str(mo)))
+
+
 
