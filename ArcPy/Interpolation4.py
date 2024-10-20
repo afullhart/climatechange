c5920ba8f8ef855440179990e0cba5fa02284cfc
afullhart <58483164+afullhart@users.noreply.github.com>
2024-10-20 00:13:03 -0700

    Update Interpolation4.py

--- ArcPy/Interpolation4.py
+++ ArcPy/Interpolation4.py
@@ -1,3 +1,4 @@
+
 ###############################################################################
 'MX5P surfaces from gradient boosting'
 ###############################################################################
@@ -26,8 +27,8 @@ arcpy.env.overwriteOutput = True
 arcpy.env.randomGenerator = '123 ACM599'
 
 var_labels = ['mx5p']
-covar_labels = ['accm', 'tmax', 'srad', 'DEM']
-year_labels = ['_1974_2013_', '_2000_2029_', '_2010_2039_', '_2020_2049_', '_2030_2059_', '_2040_2069_', '_2050_2079_', '_2060_2089_', '_2070_2099_']
+covar_labels = ['accm', 'tmax', 'srad']
+year_labels = ['1974_2013', '2000_2029', '2010_2039', '2020_2049', '2030_2059', '2040_2069', '2050_2079', '2060_2089', '2070_2099']
 
 extent = [43.0, -121.0, -102.0, 30.0]
 
@@ -35,11 +36,10 @@ map_io_data = []
 for mo in range(1, 13):
   ground = 'MX5P_{}'.format(mo) + '.txt'
   for yrlabel in year_labels:
-    covars = [var + yrlabel + str(mo) for var in covar_labels[:-1]]
-    covars.append(covar_labels[-1])
+    covars = [var + '_' + yrlabel + '_' + str(mo) for var in covar_labels]
     map_io_data.append([ground, covars])
 
-rasterD = os.path.join(dataDIR, covars[3] + '.tif')
+rasterD = os.path.join(dataDIR, 'DEM.tif')
 outputCoordinateSystem = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]'
 snapRaster = rasterD
 parallelProcessingFactor = '5'
@@ -56,13 +56,13 @@ with open(os.path.join(dataDIR, 'GB_CV.csv'), 'w') as fo:
     covars = io[1]
     
     featAA = os.path.join(featDIR, ground)
-    rasterDD = os.path.join(elevDIR, covars[3] + '.tif')
+    rasterDD = os.path.join(elevDIR, 'DEM.tif')
     
     featA = os.path.join(dataDIR, ground)
     rasterA = covars[0]
     rasterB = covars[1]
     rasterC = covars[2]
-    rasterD = os.path.join(dataDIR, covars[3] + '.tif')
+    rasterD = os.path.join(dataDIR, 'DEM.tif')
     
     shutil.copyfile(featAA, featA)
     shutil.copyfile(rasterDD, rasterD)
@@ -71,7 +71,7 @@ with open(os.path.join(dataDIR, 'GB_CV.csv'), 'w') as fo:
     yrs_i = int((i) % 9)
     yrs = year_labels[yrs_i]
     
-    if yrs == '_1974_2013_':
+    if yrs == '1974_2013':
       
       arcpy.management.XYTableToPoint(
         in_table=featA,
@@ -91,7 +91,7 @@ with open(os.path.join(dataDIR, 'GB_CV.csv'), 'w') as fo:
           treat_variable_as_categorical=None,
           explanatory_variables=None,
           distance_features=None,
-          explanatory_rasters='{} #;srad_1974_2013_{} #;tmax_1974_2013_{} #;accm_1974_2013_{} #'.format(os.path.join(dataDIR, 'DEM.tif'), mo, mo, mo),
+          explanatory_rasters='srad_1974_2013_{} #;tmax_1974_2013_{} #;accm_1974_2013_{} #'.format(mo, mo, mo),
           features_to_predict=None,
           output_features=None,
           output_raster=None,
@@ -137,7 +137,7 @@ with open(os.path.join(dataDIR, 'GB_CV.csv'), 'w') as fo:
           treat_variable_as_categorical=None,
           explanatory_variables=None,
           distance_features=None,
-          explanatory_rasters='{} #;srad_1974_2013_{} #;tmax_1974_2013_{} #;accm_1974_2013_{} #'.format(os.path.join(dataDIR, 'DEM.tif'), mo, mo, mo),
+          explanatory_rasters='srad_1974_2013_{} #;tmax_1974_2013_{} #;accm_1974_2013_{} #'.format(mo, mo, mo),
           features_to_predict=None,
           output_features=None,
           output_raster=None,
@@ -173,7 +173,7 @@ with open(os.path.join(dataDIR, 'GB_CV.csv'), 'w') as fo:
         pbias_diff = 100*(np.sum(df['DATA'] - df['PREDICTED'])/np.sum(df['DATA']))
         mape_diff = 100*(1/df['DATA'].loc[df['DATA'] != 0.0].size)*(np.nansum(np.absolute((df['DATA'] - df['PREDICTED'])/(df['DATA'].replace(0.0, np.nan)))))
                      
-        fo.write('MX5P{}{}'.format(yrs, mo) + ',' + str(rmse_diff) + ',' + str(pbias_diff) + ',' + str(mape_diff) + '\n')
+        fo.write('mx5p_{}_{}'.format(yrs, mo) + ',' + str(rmse_diff) + ',' + str(pbias_diff) + ',' + str(mape_diff) + '\n')
 
     with arcpy.EnvManager(outputCoordinateSystem=outputCoordinateSystem, extent=extent, snapRaster=snapRaster, parallelProcessingFactor=parallelProcessingFactor, cellSize=cellSize):
 
@@ -181,12 +181,12 @@ with open(os.path.join(dataDIR, 'GB_CV.csv'), 'w') as fo:
         input_model=os.path.join(dataDIR, 'TrainedModel.ssm'),
         prediction_type='PREDICT_RASTER',
         features_to_predict=None,
-        output_raster=os.path.join(gdbDIR, 'MX5P{}{}'.format(yrs, mo)),
-        explanatory_rasters_matching='{} DEM false;srad{}{} SRAD_1974_2013_{} false;tmax{}{} TMAX_1974_2013_{} false;accm{}{} ACCM_1974_2013_{} false'.format(os.path.join(dataDIR, 'DEM.tif'), yrs, mo, mo, yrs, mo, mo, yrs, mo, mo)
+        output_raster=os.path.join(gdbDIR, 'mx5p_{}_{}'.format(yrs, mo)),
+        explanatory_rasters_matching='SRAD_{}_{} SRAD_1974_2013_{} false;TMAX_{}_{} TMAX_1974_2013_{} false;ACCM_{}_{} ACCM_1974_2013_{} false'.format(yrs, mo, mo, yrs, mo, mo, yrs, mo, mo)
       )
   
-    outExtractByMask = arcpy.sa.ExtractByMask(os.path.join(gdbDIR, 'MX5P{}{}'.format(yrs, mo)), maskSHP, 'INSIDE')
-    outExtractByMask.save(os.path.join(gdbDIR, 'mx5p{}{}'.format(yrs, mo)))
+    outExtractByMask = arcpy.sa.ExtractByMask(os.path.join(gdbDIR, 'mx5p_{}_{}'.format(yrs, mo)), maskSHP, 'INSIDE')
+    outExtractByMask.save(os.path.join(gdbDIR, 'mx5p_{}_{}'.format(yrs, mo)))
 
   arcpy.management.Delete(os.path.join(gdbDIR, 'MX5P_{}_XYTableToPoint'.format(mo)))
   arcpy.management.Delete('GBfeatures')
@@ -197,8 +197,5 @@ with open(os.path.join(dataDIR, 'GB_CV.csv'), 'w') as fo:
 
 for mo in range(1, 13):
   arcpy.management.Delete(os.path.join(gdbDIR, 'MX5P_{}_XYTableToPoint'.format(mo)))
-
-
-
 
 
