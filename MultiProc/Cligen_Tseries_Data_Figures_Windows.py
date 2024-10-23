054a47fd92c983b8ba8052b425fdeb654f49e2c5
afullhart <58483164+afullhart@users.noreply.github.com>
2024-10-22 22:47:00 -0700

    Add files via upload

---
+++ MultiProc/Cligen_Tseries_Data_Figures_Windows.py
@@ -0,0 +1,308 @@
+
+wind_str = """% N      10.99 11.41  9.45  7.02  5.78  5.06  5.28  6.93  9.04  9.89 10.90 10.59
+MEAN      2.47  2.78  2.76  2.60  2.55  2.52  2.80  2.80  2.64  2.48  2.66  2.43
+STD DEV   1.30  1.69  1.52  1.25  1.15  1.22  1.44  1.54  1.19  1.18  1.68  1.29
+SKEW      1.68  2.18  1.61  1.31   .86  1.38  1.26  1.64  1.07  1.26  2.15  1.35
+% NNE     4.27  3.83  3.55  3.14  2.29  1.94  2.25  2.66  3.62  4.16  4.19  4.07
+MEAN      2.98  2.82  2.97  2.62  2.45  2.56  3.00  2.77  2.60  2.63  3.09  2.78
+STD DEV   1.91  1.73  1.81  1.31  1.24  1.27  1.65  1.56  1.33  1.44  1.91  1.59
+SKEW      1.33  1.68  1.76  1.01  1.62  1.20  1.01  1.61  1.42  1.67  1.37  1.41
+% NE      7.71  7.27  6.51  5.97  4.96  4.28  4.55  5.39  6.58  7.62  8.33  7.86
+MEAN      2.66  2.69  2.38  2.50  2.44  2.22  2.55  2.56  2.47  2.54  2.78  2.62
+STD DEV   1.50  1.55  1.14  1.21  1.26   .98  1.36  1.35  1.20  1.33  1.64  1.42
+SKEW      1.29  1.50  1.24  1.28  1.66  1.09  1.52  1.67  1.38  1.35  1.46  1.21
+% ENE     4.46  3.84  3.08  2.99  2.68  2.27  2.28  2.85  4.18  4.25  4.46  4.23
+MEAN      2.69  2.87  2.66  2.60  2.67  2.42  2.85  2.68  2.84  2.80  3.04  2.72
+STD DEV   1.38  1.56  1.29  1.29  1.38  1.10  1.48  1.28  1.48  1.48  1.79  1.42
+SKEW      1.16  1.43  1.17  1.09  1.32  1.04  1.40  1.23  1.26  1.27  1.41  1.06
+% E      16.03 14.27 11.89 11.42  9.87  8.99  8.21 10.53 13.88 15.79 14.99 15.97
+MEAN      2.62  2.74  2.65  2.71  2.77  2.63  3.03  3.01  2.90  2.90  2.73  2.61
+STD DEV   1.33  1.36  1.24  1.29  1.34  1.26  1.50  1.47  1.41  1.44  1.36  1.25
+SKEW      1.27  1.06   .97  1.00   .93   .99  1.09  1.08  1.05  1.08  1.24   .99
+% ESE     5.82  5.68  5.30  4.79  4.81  4.29  4.38  5.36  6.57  6.77  5.79  5.94
+MEAN      2.63  2.80  2.75  3.02  3.04  2.86  3.32  3.28  3.33  3.02  2.64  2.55
+STD DEV   1.26  1.25  1.16  1.44  1.41  1.28  1.58  1.64  1.59  1.37  1.21  1.16
+SKEW       .95   .75   .58   .80   .76   .96   .96  1.16   .83   .83   .90   .91
+% SE      5.75  5.57  5.59  5.17  5.06  5.16  6.10  7.08  6.81  6.26  5.42  5.08
+MEAN      2.30  2.46  2.62  2.65  2.82  2.82  3.40  3.17  3.08  2.78  2.46  2.33
+STD DEV   1.09  1.18  1.21  1.28  1.35  1.47  1.83  1.75  1.65  1.32  1.24  1.01
+SKEW      1.10  1.21   .85  1.42   .96  1.18  1.16  1.45  1.14   .80  1.21   .72
+% SSE     1.73  1.81  1.86  1.81  1.78  1.90  2.13  2.53  2.10  1.84  1.57  1.52
+MEAN      2.20  2.46  2.85  2.61  2.98  2.89  3.35  2.95  2.85  2.81  2.22  1.95
+STD DEV   1.02  1.23  1.33  1.19  1.46  1.35  1.65  1.45  1.27  1.31  1.09   .83
+SKEW      1.05   .73   .66   .72   .72   .52   .87  1.45   .59   .49   .99  1.23
+% S       3.28  4.11  4.90  4.88  5.27  5.32  5.38  5.02  3.53  3.55  3.10  2.81
+MEAN      2.15  2.48  3.12  3.24  3.39  3.42  3.48  3.05  2.88  2.73  2.50  2.24
+STD DEV   1.13  1.35  1.78  1.76  1.87  1.85  1.89  1.66  1.53  1.53  1.49  1.18
+SKEW      1.36  1.43  1.11   .88  1.00   .88   .91  1.24  1.12  1.10  1.59  1.40
+% SSW     1.76  2.57  3.93  5.27  6.22  6.04  5.56  4.15  2.92  2.33  1.98  2.01
+MEAN      2.75  2.92  3.79  4.16  3.98  3.94  3.54  3.48  3.43  3.41  2.67  2.64
+STD DEV   1.50  1.50  1.94  2.04  1.86  1.89  1.52  1.66  1.63  1.70  1.28  1.35
+SKEW      1.02  1.01  1.05   .73   .57   .64   .53   .86   .81   .82  1.05   .87
+% SW      4.07  5.49  8.28 11.06 12.70 12.08 10.57  7.97  5.46  4.57  4.05  4.18
+MEAN      2.88  3.30  4.04  4.60  4.46  4.22  3.90  3.71  3.55  3.41  3.03  3.08
+STD DEV   1.51  1.76  2.07  2.40  2.13  1.99  1.75  1.71  1.74  1.80  1.60  1.71
+SKEW       .98   .99   .87   .78   .66   .67   .55   .82   .98  1.15  1.03   .98
+% WSW     2.79  3.88  5.52  7.05  7.88  7.43  6.91  5.29  3.67  2.88  2.66  2.43
+MEAN      3.12  4.01  4.59  4.91  4.86  4.47  4.16  3.76  3.88  3.65  3.29  3.43
+STD DEV   1.76  2.17  2.39  2.38  2.22  1.97  1.77  1.58  1.85  1.91  1.65  1.98
+SKEW      1.08   .87   .88   .50   .50   .60   .37   .38   .72   .90   .70   .87
+% W       4.93  6.85  8.42 10.11 11.11 13.23 14.52 11.14  7.44  5.65  5.11  4.59
+MEAN      3.25  3.90  4.30  4.29  4.19  3.99  3.81  3.48  3.40  3.19  3.17  3.18
+STD DEV   1.91  2.50  2.39  2.28  2.04  1.79  1.69  1.55  1.52  1.70  1.86  2.04
+SKEW      1.07  1.10   .85   .75   .76   .54   .57   .58   .76  1.04  1.23  1.30
+% WNW     1.91  2.44  2.90  3.23  3.51  3.98  4.11  3.19  2.65  1.88  1.73  1.88
+MEAN      3.04  3.96  4.32  4.13  3.89  3.52  3.46  3.36  3.06  3.04  3.04  2.83
+STD DEV   2.03  2.48  2.61  2.22  1.93  1.54  1.42  1.49  1.27  1.70  2.00  1.77
+SKEW      1.42   .91   .89   .75   .81   .39   .39   .67   .35  1.50  1.35  1.25
+% NW      2.84  3.42  3.90  4.03  3.99  4.77  5.41  5.09  4.22  3.19  3.02  2.92
+MEAN      2.85  3.15  3.28  3.19  3.11  2.95  3.06  3.02  2.71  2.58  2.47  2.68
+STD DEV   2.11  2.08  1.96  1.77  1.52  1.36  1.40  1.49  1.18  1.31  1.44  1.70
+SKEW      2.08  1.47  1.27  1.05   .88   .75   .64  1.05   .66  1.31  1.53  1.50
+% NNW     3.14  3.30  3.17  2.66  2.03  2.40  2.62  2.86  3.02  3.01  3.25  3.31
+MEAN      2.58  2.92  3.14  2.65  2.91  2.74  2.97  3.04  2.74  2.63  2.64  2.52
+STD DEV   1.35  1.59  1.71  1.14  1.44  1.27  1.44  1.49  1.10  1.20  1.31  1.16
+SKEW      1.26  1.35  1.22   .65   .75  1.07  1.12  1.39   .39   .89  1.26  1.21
+CALM     18.55 14.26 11.69  9.36 10.09 10.88  9.71 11.98 14.30 16.41 19.50 20.64
+ """
+
+
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import subprocess as sub
+import os
+import pandas as pd
+from classes.Formatting import Formatting
+formatting_obj = Formatting()
+import shutil
+from subprocess import Popen, PIPE
+from osgeo import ogr
+from osgeo import gdal
+from scipy import optimize
+import numpy as np
+
+gdbDIR = r'C:\Users\afullhart\Documents\ArcGIS\Projects\CCSM4\CCSM4.gdb'
+cliDIR = r'C:\Users\afullhart\Desktop\cligenv53'
+ptFILE = r'C:\Users\afullhart\Desktop\Points.csv'
+outFILE = r'C:\Users\afullhart\Desktop\CCSM4_Output_Data.csv'
+
+var_labels = ['mean', 'sdev', 'skew', 'pww', 'pwd', 'tmax', 'tmin', 'txsd', 'tnsd', 'srad', 'srsd', 'mx5p', 'tdew', 'timepk']
+year_labels = ['1974_2013', '2000_2029', '2010_2039', '2020_2049', '2030_2059', '2040_2069', '2050_2079', '2060_2089', '2070_2099']
+historical_var_labels = ['ratio', 'timepk']
+
+yr_str = '1974_2013'
+REC_LEN = 100
+eo = 0.29; a = 0.72; io = 12.195
+
+with open(ptFILE) as f:
+  point_list = []
+  next(f)
+  for line in f:
+    row = line.split(',')
+    i = int(row[0])
+    x = float(row[1])
+    y = float(row[2])
+    point_list.append([i, x, y])
+
+point_list = point_list[:100]
+
+def coord2pixel(x, y, trans):
+  xpt = (((x - trans[0]) / trans[1]))
+  ypt = (((y - trans[3]) / trans[5]))
+  return(xpt, ypt)
+
+def newton(b_, ip_):
+  return (ip_ * (1 - np.exp(-b_))) - (b_)
+
+def i30(p_, ip_, d_, b_):
+  return ((2*p_*ip_)/(b_)) * (1 - np.exp(-((b_)/(2*d_))))
+
+def energy(p_, ip_, lp_, b_, eo_, a_, io_,):
+  inside = np.exp( -(lp_ / io_)*np.exp( -b_ ) ) - np.exp( -lp_ / io_ )
+  middle = ((a_*ip_)/(b_)) * (io_/lp_)
+  outside = p_*eo_
+  return (outside)*((1)-(middle*inside))
+
+def main(point):
+  
+  fname = str(point[0])
+  
+  par_df = pd.DataFrame(columns=range(1, 12), index=var_labels)
+
+  for varlb in var_labels:
+    
+    if varlb not in historical_var_labels:
+      raster_name = varlb + '_' + yr_str
+    else:
+      raster_name = varlb + '_1974_2013'
+
+    raster = gdal.Open(f'OpenFileGDB:{gdbDIR}:{raster_name}')
+    transform = raster.GetGeoTransform()
+    xp, yp = coord2pixel(point[1], point[2], transform)
+
+    for mo in range(1, 13):
+      
+      band = raster.GetRasterBand(mo)
+      pixel_value = band.ReadAsArray(int(xp), int(yp), 1, 1)[0, 0] 
+      par_df.at[varlb, mo] = pixel_value  
+      band = None
+  
+    raster = None
+
+  par_df.loc['tmax'] = par_df.loc['tmax'].apply(lambda x: x*(9.0/5.0) + 32.0)
+  par_df.loc['tmin'] = par_df.loc['tmin'].apply(lambda x: x*(9.0/5.0) + 32.0)
+  par_df.loc['tdew'] = par_df.loc['tdew'].apply(lambda x: x*(9.0/5.0) + 32.0)
+  par_df.loc['txsd'] = par_df.loc['txsd'].apply(lambda x: x*(9.0/5.0))
+  par_df.loc['tnsd'] = par_df.loc['tnsd'].apply(lambda x: x*(9.0/5.0))
+  par_df.loc['srad'] = par_df.loc['srad'].apply(lambda x: x*86400.0/41868.0)
+  par_df.loc['srsd'] = par_df.loc['srsd'].apply(lambda x: x*86400.0/41868.0)
+  
+  meanP_list = par_df.loc['mean']
+  sdevP_list = par_df.loc['sdev']
+  skewP_list = par_df.loc['skew']
+  ww_list = par_df.loc['pww']
+  wd_list = par_df.loc['pwd']
+  tmax_list = par_df.loc['tmax']
+  tmin_list = par_df.loc['tmin']
+  sdtmax_list = par_df.loc['txsd']
+  sdtmin_list = par_df.loc['tnsd']
+  solrad_list = par_df.loc['srad']
+  solsdev_list = par_df.loc['srsd']
+  mx5p_list = par_df.loc['mx5p']
+  dewpt_list = par_df.loc['tdew']
+  timepk_list = par_df.loc['timepk']
+
+  meanP, sdevP, skewP, ww, wd = [], [], [], [], []
+  tmax, tmin, sdtmax, sdtmin = [], [], [], []
+  solrad, solsdev, mx5p, dewpt, timepk = [], [], [], [], []
+
+  for mo in range(1, 13):
+    meanP.append('{:.2f}'.format(meanP_list[mo]).lstrip('0'))
+    sdevP.append('{:.2f}'.format(sdevP_list[mo]).lstrip('0'))
+    skewP.append('{:.2f}'.format(skewP_list[mo]).lstrip('0'))
+    ww.append('{:.2f}'.format(ww_list[mo]).lstrip('0'))
+    wd.append('{:.2f}'.format(wd_list[mo]).lstrip('0'))
+    tmax.append('{:.2f}'.format(tmax_list[mo]).lstrip('0'))
+    tmin.append('{:.2f}'.format(tmin_list[mo]).lstrip('0'))
+    sdtmax.append('{:.2f}'.format(sdtmax_list[mo]).lstrip('0'))
+    sdtmin.append('{:.2f}'.format(sdtmin_list[mo]).lstrip('0')) 
+    solrad.append('%.0f' %round(float(solrad_list[mo]), 0) + '.')
+    solsdev.append('%.1f' %round(float(solsdev_list[mo]), 1))
+    mx5p.append(('%.2f' %round(float(mx5p_list[mo]), 2)).lstrip('0'))
+    dewpt.append('%.2f' %round(float(dewpt_list[mo]), 2))
+    timepk.append(('%.3f' %round(float(timepk_list[mo]), 3)).lstrip('0'))
+
+  with open(os.path.join(cliDIR, fname + '.par'), 'w') as f_out: 
+    lat, lon = 40.0, -110.0
+    title = ' Grid point'
+    yrsStr = 'YEARS= 30. TYPE= 1'
+    tpStr = 'TP5 = 1.23 TP6= 2.34' 
+
+    lat, lon = '%.2f' % round(float(lat), 2), '%.2f' % round(float(lon), 2)
+    elev = 123.0
+    elev = '%.0f' % round(float(elev), 0) + '.'
+
+    f_out.write(title + '\n' )
+    f_out.write(' LATT=' + formatting_obj.spacing_lat_lon(lat) + lat)
+    f_out.write(' LONG=' + formatting_obj.spacing_lat_lon(lon) + lon)
+    f_out.write(' ' + yrsStr + '\n')
+    f_out.write(' ELEVATION =' + formatting_obj.spacing_elev(elev) + elev)
+    f_out.write(' ' + tpStr + '\n')
+
+    gen = formatting_obj.spacing_gen_new( meanP )
+    f_out.write( ' MEAN P ' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(meanP)] ) + '\n' )
+    gen = formatting_obj.spacing_gen_new( sdevP )
+    f_out.write( ' S DEV P' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(sdevP)] ) + '\n' )
+    gen = formatting_obj.spacing_gen_new( skewP )
+    f_out.write( ' SKEW  P' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(skewP)] ) + '\n' )
+    gen = formatting_obj.spacing_gen_new( ww )
+    f_out.write( ' P(W/W) ' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(ww)] ) + '\n' )
+    gen = formatting_obj.spacing_gen_new( wd )
+    f_out.write( ' P(W/D) ' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(wd)] ) + '\n' )
+    gen = formatting_obj.spacing_gen_new( tmax )
+    f_out.write( ' TMAX AV' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(tmax)] ) + '\n' )
+    gen = formatting_obj.spacing_gen_new( tmin )
+    f_out.write( ' TMIN AV' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(tmin)] ) + '\n' )
+    gen = formatting_obj.spacing_gen_new( sdtmax )
+    f_out.write( ' SD TMAX' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(sdtmax)] ) + '\n' )
+    gen = formatting_obj.spacing_gen_new( sdtmin )
+    f_out.write( ' SD TMIN' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(sdtmin)] ) + '\n' )
+    f_out.write(' SOL.RAD  191.  274.  371.  487.  603.  678.  687.  594.  462.  323.  189.  161.\n')
+    f_out.write(' SD SOL   50.0  68.1  90.3 103.9 105.7  87.4  48.7  60.6  66.4  78.5  56.2  36.9\n')
+    gen = formatting_obj.spacing_gen_new( mx5p )
+    f_out.write( ' MX .5 P' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(mx5p)] ) + '\n' )
+    f_out.write(' DEW PT  39.56 41.56 42.67 41.73 44.73 47.77 52.71 52.67 50.65 45.61 42.51 38.55\n')
+    gen = formatting_obj.spacing_gen_new( timepk )
+    f_out.write( 'Time Pk ' + ''.join( [next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(timepk)] ) + '\n' )    
+    
+    f_out.write(wind_str)
+
+  args = 'cligen53 -b1 -y{} -t5 -i{} -o{}'.format(REC_LEN, fname + '.par', fname + '.txt')
+  sub.run(args=args, check=False, capture_output=False, text=False, shell=True, stdout=sub.DEVNULL, stderr=sub.DEVNULL, cwd=cliDIR)
+
+  with open(os.path.join(cliDIR, fname + '.txt')) as f:
+    lines = f.readlines()[15:]
+  
+  os.remove(os.path.join(cliDIR, fname + '.txt'))
+  os.remove(os.path.join(cliDIR, fname + '.par'))
+  
+  ei_sum = 0.0
+  p_max = 0.0
+  accum = 0.0
+  for line in lines[:-1]:
+    row = line.split()
+    p = float(row[3])
+    tavg = (float(row[7]) + float(row[8])) / 2.
+    accum += p
+
+    if p >= 12.7 and tavg > 0.:
+        ip = float(row[6])
+        b = optimize.newton(func=newton, x0=ip, args=(ip,))
+        d = float(row[4])
+        lp = ip * (p/d)
+        if d > 0.5:
+            l30 = i30(p, ip, d, b)
+        else:
+            l30 = 2*p
+        e = energy(p, ip, lp, b, eo, a, io)
+        ei = e*l30
+        ei_sum += ei
+
+    else:
+        ei = 0.
+        ei_sum += ei
+
+    if p > p_max:
+      p_max = p
+
+  ann_ero = ei_sum / REC_LEN
+  ann_acc = accum / REC_LEN
+
+  return([point[0], point[1], point[2], ann_ero, p_max, ann_acc])
+
+
+
+if __name__ == '__main__':
+  results = []
+  with ProcessPoolExecutor(max_workers=4) as executor, open(outFILE, 'w') as f_out:
+    f_out.write('index,POINT_X,POINT_Y,EROSIVITY,100_YR_DLY,ANNUAL\n')
+    pool = {executor.submit(main, p): p for p in point_list}
+    for future in as_completed(pool):
+      res = future.result()
+      results.append(res)
+      f_out.write(','.join([str(res[0]), str(res[1]), str(res[2]), str(res[3]), str(res[4]), str(res[5])]) + '\n')
+
+
+
+
+
+# transform = raster.GetGeoTransform()
+# band = raster.GetRasterBand(1)
+# print('Band min. value', band.GetMinimum())
+# print('Band max. value', band.GetMaximum())
+# print('Band NoData value', band.GetNoDataValue())
+# driver = raster.GetDriver().LongName
+# sizeX = raster.RasterXSize
+# sizeY = raster.RasterYSize
+# count = raster.RasterCount
+# proj = raster.GetProjection()
+# info = gdal.Info(raster)
